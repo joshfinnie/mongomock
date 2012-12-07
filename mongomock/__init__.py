9f4395ab146013860a38ed080c88679666c7e474
Josh Finnie <josh@koansys.com>
2012-12-06 21:00:00 -0500

    Fixing Remove

--- mongomock/__init__.py
+++ mongomock/__init__.py
@@ -281,7 +281,7 @@ class Collection(object):
                         manipulate, safe, _check_keys=True, **kwargs)
             return to_save.get("_id", None)
 
-    def remove(self, spec_or_id=None, search_filter=None):
+    def remove(self, spec_or_id=None, safe=None, search_filter=None):
         """Remove objects matching spec_or_id from the collection."""
         if search_filter is not None:
             _print_deprecation_warning('search_filter', 'spec_or_id')
